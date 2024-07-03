1793cbdb26ed0217d280df4e8ed17808ce401f3a
jfigueirahasbun <jfigueirahasbun@sogyo.nl>
2024-07-03 14:47:51 +0200

    Add background image

--- wallet-app-expo/App.tsx
+++ wallet-app-expo/App.tsx
@@ -1,22 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, useColorScheme } from 'react-native';
-import { Colors } from 'react-native/Libraries/NewAppScreen';
+import React from 'react';
+import Welcome from './screens/Welcome';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={{ color: 'white' }}>Open up App.tsx to start working on your app!</Text>
-      <Text style={{ color: 'white' }}>Alternatively, you can just procrastinate xoxo</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Welcome />
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#000000',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ wallet-app-expo/screens/Welcome.tsx
@@ -0,0 +1,54 @@
+// In our welcome screen, we import react and functioncomponent from  react
+// the status bar, and also styled-components
+
+import React, { FunctionComponent } from "react";
+import { StatusBar } from "expo-status-bar";
+import styled from "styled-components/native";
+
+//custom components
+import { colors } from "../components/colors";
+import { Container } from "../components/shared";
+
+const WelcomeContainer = styled(Container)`
+    background-color: ${colors.secondary};
+    justify-content: space-between;
+    width: 100%;
+    height: 100%;
+`;
+
+const TopSection = styled.View`
+    width:100%;
+    flex:1;
+    max-height: 55%;
+`;
+
+const TopImage = styled.Image`
+    width:100%;
+    height: 100%;
+    resize-mode: stretch;
+`;
+
+const BottomSection = styled.View`
+    width:100%;
+    flex:1;
+    padding: 25px;
+`;
+
+import background from "./../assets/bgs/background_v1.png";
+
+const Welcome : FunctionComponent = () => {
+    return (
+        <>
+        
+        <StatusBar style="light"></StatusBar>
+        <WelcomeContainer>
+            <TopSection>
+                <TopImage source={background}/>
+            </TopSection>
+        </WelcomeContainer>
+        
+        </>
+    )
+}
+
+export default Welcome;
