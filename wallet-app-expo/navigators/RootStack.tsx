7f0cf7ec20f5e17c4cbad236582f1abbff8a9240
jfigueirahasbun <jfigueirahasbun@sogyo.nl>
2024-07-04 12:20:08 +0200

    initial home screen making

--- wallet-app-expo/navigators/RootStack.tsx
+++ wallet-app-expo/navigators/RootStack.tsx
@@ -4,9 +4,12 @@ import React, { FunctionComponent } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import Welcome from "./../screens/Welcome";
+import Home from "../screens/Home";
+import { colors } from "../components/colors";
 
 export type RootStackParamList ={
     Welcome: undefined;
+    Home: undefined;
 }
 
 const Stack = createStackNavigator();
@@ -14,11 +17,28 @@ const Stack = createStackNavigator();
 const RootStack: FunctionComponent<RootStackParamList> = () => {
     return (
     <NavigationContainer>
-        <Stack.Navigator>
+        <Stack.Navigator
+            screenOptions={{
+                headerStyle: {
+                    backgroundColor: colors.graylight,
+                    borderBottomWidth:0,
+                    shadowColor: "transparent",
+                    shadowOpacity:0,
+                    elevation:0,
+                    height:120,
+                },
+                headerTintColor:colors.secondary,
+            }}
+            initialRouteName="Welcome">
             <Stack.Screen
                 name="Welcome"
                 component={Welcome}
                 options={{headerShown: false}}
+            />
+            <Stack.Screen
+                name="Home"
+                component={Home}
+                options={{headerShown: true}}
             />
         </Stack.Navigator>
     </NavigationContainer>
