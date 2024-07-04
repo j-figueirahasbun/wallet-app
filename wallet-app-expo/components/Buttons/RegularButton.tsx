d2103f9f080c29785ed31037b08a481b4826d779
jfigueirahasbun <jfigueirahasbun@sogyo.nl>
2024-07-04 10:38:19 +0200

    finish welcome page with adding button to further redirect

--- wallet-app-expo/components/Buttons/RegularButton.tsx
+++ wallet-app-expo/components/Buttons/RegularButton.tsx
@@ -1,7 +1,16 @@
-import { FunctionComponent } from "react";
+import React, { FunctionComponent } from "react";
 import styled from "styled-components/native";
 import { colors } from "../colors";
+import RegularText from "../Texts/RegularText";
+import { Button, GestureResponderEvent, StyleProp, TextStyle, ViewStyle } from "react-native";
 
+//types
+interface ButtonProps {
+    btnStyles?: StyleProp<ViewStyle>;
+    onPress: ((event: GestureResponderEvent) => void) | undefined;
+    textStyles?: StyleProp<TextStyle>;
+    children: React.ReactNode;
+}
 
 const ButtonView = styled.TouchableOpacity`
     align-items:center;
@@ -12,8 +21,11 @@ const ButtonView = styled.TouchableOpacity`
     
 `;
 
-const RegularButton: FunctionComponent = () => {
-    return <></>;
+const RegularButton: FunctionComponent<ButtonProps> = (props) => {
+    return (
+        <ButtonView onPress={props.onPress} style={props.btnStyles}>
+            <RegularText textStyles={props.textStyles}>{props.children}</RegularText>
+        </ButtonView>);
 };
 
 export default RegularButton;
--- wallet-app-expo/screens/Welcome.tsx
+++ wallet-app-expo/screens/Welcome.tsx
@@ -32,11 +32,13 @@ const BottomSection = styled.View`
     width:100%;
     flex:1;
     padding: 25px;
+    justify-content: flex-end;
 `;
 
 import background from "./../assets/bgs/background_v1.png";
 import BigText from "../components/Texts/BigText";
 import SmallText from "../components/Texts/SmallText";
+import RegularButton from "../components/Buttons/RegularButton";
 
 const Welcome : FunctionComponent = () => {
     return (
@@ -54,6 +56,9 @@ const Welcome : FunctionComponent = () => {
                 <SmallText textStyles={{ width: "70%", marginBottom: 25}}>
                     Best payment method, connects your money to your friends!
                 </SmallText>
+                <RegularButton onPress={()=>{}}>
+                    Get Started
+                </RegularButton>
             </BottomSection>
         </WelcomeContainer>
         
