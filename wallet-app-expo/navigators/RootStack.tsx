875d2d58063ba9d125a751ef1ceb26b90f6e20f1
jfigueirahasbun <jfigueirahasbun@sogyo.nl>
2024-07-04 15:21:32 +0200

    Add greeting and profile to homescreen

--- wallet-app-expo/navigators/RootStack.tsx
+++ wallet-app-expo/navigators/RootStack.tsx
@@ -6,6 +6,9 @@ import { createStackNavigator } from "@react-navigation/stack";
 import Welcome from "./../screens/Welcome";
 import Home from "../screens/Home";
 import { colors } from "../components/colors";
+import Greeting from "../components/Header/Greeting";
+import Profile from "../components/Header/Profile";
+import Avi from "./../assets/avi/avatar.png"
 
 export type RootStackParamList ={
     Welcome: undefined;
@@ -28,8 +31,19 @@ const RootStack: FunctionComponent<RootStackParamList> = () => {
                     height:120,
                 },
                 headerTintColor:colors.secondary,
+                headerRightContainerStyle: {
+                    paddingRight: 25,
+                },
+                headerLeftContainerStyle: {
+                    paddingLeft: 10,
+                },
+                headerRight: () => (
+                    <Profile
+                        img={Avi}
+                        imgContainerStyle={{backgroundColor : colors.tertiary}}/>
+                ),
             }}
-            initialRouteName="Welcome">
+            initialRouteName="Home">
             <Stack.Screen
                 name="Welcome"
                 component={Welcome}
@@ -38,7 +52,16 @@ const RootStack: FunctionComponent<RootStackParamList> = () => {
             <Stack.Screen
                 name="Home"
                 component={Home}
-                options={{headerShown: true}}
+                options={{
+                    headerTitle : (props) => (
+                        <Greeting
+                        mainText="Hey John!"
+                        subText="Welcome Back!"
+                        {...props}
+                        />
+                    ),
+                    headerLeft: () => <></>
+                }}
             />
         </Stack.Navigator>
     </NavigationContainer>
