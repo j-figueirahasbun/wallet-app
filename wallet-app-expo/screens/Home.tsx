23beb887c11659564b74fcd95785fe21ddef5602
jfigueirahasbun <jfigueirahasbun@sogyo.nl>
2024-07-05 14:47:24 +0200

    Add cards to homescreen view

--- wallet-app-expo/screens/Home.tsx
+++ wallet-app-expo/screens/Home.tsx
@@ -5,6 +5,10 @@ import styled from "styled-components/native";
 import { colors } from "../components/colors";
 import { StatusBar } from "expo-status-bar";
 
+import CardSection from "../components/Cards.tsx/CardSection";
+import logo1 from "./../assets/cards/mc.png";
+import logo2 from "./../assets/cards/visa_white.png";
+
 const HomeContainer = styled(Container)`
     background-color: ${colors.graylight};
     width: 100%;
@@ -13,11 +17,34 @@ const HomeContainer = styled(Container)`
 `;
 
 const Home: FunctionComponent = () => {
+    const cardsData = [
+        {
+            id:1,
+            accountNo: "32472938479",
+            balance: 2348749.14,
+            alias: "Work Debit",
+            logo: logo1,
+        },
+        {
+            id:2,
+            accountNo: "2376482376",
+            balance: 15000.14,
+            alias: "Personal PrePaid",
+            logo: logo2,
+        },
+        {
+            id:3,
+            accountNo: "92376487",
+            balance: 300.14,
+            alias: "School Prepaid",
+            logo: logo1,
+        },
+    ]
+    
     return(
         <HomeContainer>
-            <StatusBar>
-                
-            </StatusBar>
+            <StatusBar style="dark"/>
+            <CardSection data={cardsData}/>
         </HomeContainer>
     )
 }
